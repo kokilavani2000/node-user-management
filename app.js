8fcdf931a3557c4cb8a156a7b53b514017288639
kokilavani <webwizardsusa@gmail.com>
2026-02-09 14:49:16 +0530

    basic index view setup  and theme update

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const express = require('express');
+const path = require('path');
+
 const app = express();
 
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
-app.listen(3000, () => {
-    console.log('server is running on https://localhost:3000')
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.render('index');
 });
 
+app.listen(3000, () => {
+    console.log('Server running on http://localhost:3000');
+});
